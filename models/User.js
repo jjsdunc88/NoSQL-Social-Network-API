0ec0f7b4825dd303d479d75741ae7061eedcfa1a
JDunc <jjsduncan@gmail.com>
2023-07-26 17:31:04 -0400

    add starter files, add user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+
+
+)
+
+userSchema.virtual('friendCount').get (function () {
+    return this.friends.length;
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = Student;
