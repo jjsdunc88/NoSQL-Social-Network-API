68c07de9a3e6ae1cc2abc627472af6ac9a75337c
JDunc <jjsduncan@gmail.com>
2023-07-28 19:13:52 -0400

    define reactionSchema on Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,40 @@
+const { Schema, Types, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dayjs = require('dayjs');
+
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: function (dateTime) {
+                return dayjs(dateTime).format("MM/DD/YYYY hh:mm:ss A")
+            },
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+
+    
+);
+
+module.exports = reactionSchema;
