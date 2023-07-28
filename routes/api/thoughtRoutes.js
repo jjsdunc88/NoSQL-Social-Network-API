7ad6e0d592b635d6304b00c9fa7c0a7f0ec3c731
JDunc <jjsduncan@gmail.com>
2023-07-28 19:58:11 -0400

    add routes to thoughtRoutes.js in API folder

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getOneThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
