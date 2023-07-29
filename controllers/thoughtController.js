9d1a885df002c66502599d090c9cd3686548659b
JDunc <jjsduncan@gmail.com>
2023-07-28 23:05:19 -0400

    add updateThought and deleteThought functions to thoughtControllers.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -30,7 +30,7 @@ module.exports = {
 
     // Create Thought
     async createThought(req, res) {
-        try { 
+        try {
             const thought = await Thought.create(req.body);
             const user = await User.findOneAndUpdate(
                 { _id: req.body.userId },
@@ -41,13 +41,57 @@ module.exports = {
             if (!user) {
                 return res.status(404).json({ message: "Thought created, User not found!" });
             }
-            res.json("Thought created!");
+            res.json({ message: "Thought created!" });
         } catch (err) {
             res.status(500).json(err);
         }
     },
 
     //Update Thought
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: "Thought not found!" });
+            }
+            res.json({ message: "Thought updated!" });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Delete Thought
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndRemove({
+                _id: req.params.thoughtId,
+            });
+
+            if (!thought) {
+                return res.status(404).json({ message: "Thought not found!" });
+            }
+
+            const user = await User.findOneAndUpdate(
+                { thoughts: req.params.thoughtId },
+                { $pull: { thoughts: req.params.thoughtId } },
+                { new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found!" });
+            }
+
+            res.json({ message: "Thought deleted!" });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
 
 
 
