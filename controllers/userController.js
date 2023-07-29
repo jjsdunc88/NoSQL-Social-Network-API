6570e5ae8c463b911b56a6b9a30d0d3610418f23
JDunc <jjsduncan@gmail.com>
2023-07-28 21:09:29 -0400

    add create and update user functions to userControllers.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,4 +28,39 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    // Create a new user
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Update existing user
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found!" });
+            }
+            res.json({ message: "User updated!" });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+
+
+
+
+
+
 }
