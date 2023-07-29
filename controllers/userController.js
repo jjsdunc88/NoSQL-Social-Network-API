e80e4380f58d2f904def81f03e5c37e6415b282b
JDunc <jjsduncan@gmail.com>
2023-07-28 21:30:29 -0400

    add deleteUser function on userControllers.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const { User, Thought } = require('../models')
 
 module.exports = {
 
-    // Get all users
+    // Get all Users
     async getUsers(req, res) {
         try {
             const users = await User.find();
@@ -12,7 +12,7 @@ module.exports = {
         }
     },
 
-    // Get one user
+    // Get one User
     async getOneUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userID }).select(
@@ -29,7 +29,7 @@ module.exports = {
         }
     },
 
-    // Create a new user
+    // Create a new User
     async createUser(req, res) {
         try {
             const dbUserData = await User.create(req.body);
@@ -39,7 +39,7 @@ module.exports = {
         }
     },
 
-    // Update existing user
+    // Update existing User
     async updateUser(req, res) {
         try {
             const user = await User.findOneAndUpdate(
@@ -56,6 +56,29 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    // Delete User and thoughts
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findByIdAndRemove(req.params.userId);
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found!" });
+            }
+
+            const thought = await Thought.deleteMany({
+                username: user.username,
+            });
+
+            res.json({ message: "User deleted!" });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Add friend to User
+
+
 
 
 
