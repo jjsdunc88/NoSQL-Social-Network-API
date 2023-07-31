b3f1cfcac81ff5fb703318e7e44f713a65ef4234
JDunc <jjsduncan@gmail.com>
2023-07-31 15:47:40 -0400

    correct addFriend route on userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,7 +18,7 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends
-router.route(':userId/friends').post(addFriend);
+router.route('/:userId/friends').post(addFriend);
 
 // /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').delete(deleteFriend);
